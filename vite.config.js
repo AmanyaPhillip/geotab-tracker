208edd70a5b3eb65183f93f1180c182c61f0cd3e
AmanyaPhillip <amanyaphillip@outlook.com>
2025-07-31 16:18:15 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : '/geotab-tracker/'
+  // This is the critical line.
+  base: '/geotab-tracker/' 
 })
